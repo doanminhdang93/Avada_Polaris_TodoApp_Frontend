cc017418677f3aa82bf9e230d3df516b2d278bc7
Dmih_Dang9 <minhdang09032002@gmail.com>
2023-08-15 10:54:30 +0700

    done

--- src/components/tasks/TaskList.jsx
+++ src/components/tasks/TaskList.jsx
@@ -80,7 +80,7 @@ const TaskList = () => {
   const emptyStateMarkup = (
     <EmptyState
       heading="No task found!"
-      action={{ content: "Add new task !!!", onAction: toggleModal }}
+      action={{ content: "Add new task!", onAction: toggleModal }}
       image="https://cdn.shopify.com/s/files/1/0262/4071/2726/files/emptystate-files.png"
     ></EmptyState>
   );
